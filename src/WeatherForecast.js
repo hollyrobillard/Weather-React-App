d6c9a1f2ae152a621d28bd819b5f4cfe5bc468b1
Holly Ann Robillard <103912261+hollyrobillard@users.noreply.github.com>
2022-08-30 09:00:26 -0400

    Fixes forecast re-render with useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,11 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  //When coordinates change, set the variable loaded to false. In order to allow for a new API call to update the forecast. State will now be re-rendered.
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coords]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,9 +21,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+            {forecast.map(function(dailyForecast, index) {
+                if (index<5) {
+                    return(
+                        <div className="col" key={index}>
+                            <WeatherForecastDay data={dailyForecast} />
+                        </div>
+                    );
+                }
+            })}
         </div>
       </div>
     );
